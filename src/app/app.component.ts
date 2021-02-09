e3a40fc2ad042f6ec813dce0914fa3c6c2d5cd4e
Viswanath Mucheli Chenchu <mcviswanathmca@gmail.com>
2021-02-08 20:58:43 -0500

    Simple Todo Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+import { Todo } from './todo';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-hithesh';
+  title = 'Todo App';
+
+  todoValue: string;
+  list: any[] = [];
+
+  constructor(){
+    this.todoValue = "";
+  }
+
+  ngoninit(){
+    this.list = [];
+    this.todoValue = "";
+  }
+
+  addItem(){
+    if(this.todoValue !== "") {
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue="";
+  }
+
+  delItem(id:number){
+    this.list = this.list?.filter(item => item.id !== id);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from "@angular/forms";
+import { ScullyLibModule } from '@scullyio/ng-lib'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ScullyLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
